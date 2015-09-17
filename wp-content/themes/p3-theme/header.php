20e445d302b7909b6189ce929742c018b7f66412
gusmhp <guillaume.flament.pro@gmail.com>
2015-09-17 18:12:55 +0200

    seo plugin

--- wp-content/themes/p3-theme/header.php
+++ wp-content/themes/p3-theme/header.php
@@ -4,8 +4,7 @@
         <?php wp_head(); ?>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>P3</title>
-        <meta name="description" content="">
+        <title><?php wp_title(); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!--favicons-->
